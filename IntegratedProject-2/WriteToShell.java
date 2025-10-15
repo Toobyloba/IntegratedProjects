e6ae346bcd6d0f44ffd30984d8ea7eb66fb4e046
baloguntooby <balogun.tooby@gmail.com>
2025-10-15 07:21:34 +0200

    added the jar file and dockerfile for project 2

---
+++ IntegratedProject-2/WriteToShell.java
@@ -0,0 +1,19 @@
+public class WriteToShell {
+    public static void main(String[] args) {
+        // Print strings
+        System.out.println("Welcome to Java");
+        
+        // Print numbers
+        System.out.println(42);
+        System.out.println(3.14159);
+        
+        // Print variables
+        String message = "This is a variable";
+        System.out.println(message);
+        
+        // Print concatenated strings
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("The sum is: " + (num1 + num2));
+    }
+}
